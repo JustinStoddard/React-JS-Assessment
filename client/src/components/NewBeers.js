53b131090a60cb2832e72123a0c5119431a76c5e
Justin Stoddard <Stoddard.j.t@gmail.com>
2018-04-07 15:56:28 -0600

    First component works

---
+++ client/src/components/NewBeers.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const NewBeers = () => (
+  <div>
+    <h1 style={styles.text}>NewBeers Component</h1>
+  </div>
+)
+
+const styles = {
+  text: {
+    color: 'white'
+  }
+}
+
+export default NewBeers;
